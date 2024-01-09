c12ae967e23e39684d9e4d71f76d4c4763ef87b0
putma16 <54889900+putma16@users.noreply.github.com>
2024-01-09 09:59:50 +0700

    perubahan

--- app.py
+++ app.py
@@ -6,37 +6,32 @@ df = pd.read_csv('books_list.csv')
 doc_sim_df = pd.read_csv('similarity.csv')
 
 books_list = df['Title'].values
-#st.write(books_list.shape)
 
 def books_recommender(books_title, books=books_list, doc_sims=doc_sim_df):
-    # find movie id
-    #st.write(books_title.shape)
+    # find book id
     books_idx = np.where(books == books_title)[0][0]
     
-    # get movie similarities
+    # get book similarities
     books_similarities = doc_sims.iloc[books_idx].values
-    # get top 5 similar movie IDs
+    # get top 5 similar book IDs
     similar_books_idxs = np.argsort(-books_similarities)[1:6]
-    # get top 5 movies
+    # get top 5 books
     similar_books = books[similar_books_idxs]
-    # return the top 5 movies
+    # return the top 5 books
     return similar_books
 
    
 st.title('Sistem Rekomendasi Buku 📚')
-st.header('Berbasis Kemiripan Menggunakan Cosine Similiarity',divider='blue')
+st.header('Berbasis Kemiripan Menggunakan Cosine Similarity', divider='blue')
 
-user_input = st.text_input('Judul Buku', '', placeholder='Masukan judul buku')
-user_input = user_input.lower()
-
+user_input = st.selectbox('Pilih Judul Buku', books_list)
 
 if 'output' not in st.session_state:
     st.session_state.output = ''
 
 def btn_action():
-    
     try:
-        st.session_state.output = books_recommender(user_input.lower(), books_list, doc_sim_df)
+        st.session_state.output = books_recommender(user_input, books_list, doc_sim_df)
     except: 
         st.session_state.output = 'Buku yang anda inginkan belum tersedia'
     
